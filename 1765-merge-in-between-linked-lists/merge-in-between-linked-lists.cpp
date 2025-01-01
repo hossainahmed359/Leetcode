7c82da768b6e3e1aac3e7d72e1fbc2e2d1b58112
Hossain Ahmed Madani <70832248+hossainahmed359@users.noreply.github.com>
2025-01-01 21:08:56 +0600

    Time: 198 ms (65.38%) | Memory: 99.4 MB (57.62%) - LeetSync

--- 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 
 public:
@@ -32,12 +22,12 @@ public:
     }
 
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
-        ListNode* NodePrevA = getNodeAtIndex(list1, a - 1);
-        ListNode* NodeB = getNodeAtIndex(list1, b);
-        ListNode* List2TailNode = getNodeAtTail(list2);
+        ListNode* start = getNodeAtIndex(list1, a - 1);
+        ListNode* end = getNodeAtIndex(list1, b);
+        ListNode* listTwoTail = getNodeAtTail(list2);
 
-        NodePrevA->next = list2;
-        List2TailNode->next = NodeB->next;
+        start->next = list2;
+        listTwoTail->next = end->next;
 
         return list1;
     }
