6b872a9adb91653b4b12cd1381680fdb14621ae4
Hossain Ahmed Madani <70832248+hossainahmed359@users.noreply.github.com>
2025-01-09 08:00:36 +0600

    Time: 0 ms (100.00%) | Memory: 8.8 MB (28.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -39,3 +39,24 @@ public:
         return flag;
     }
 };
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char>st;
+//         for(auto ch : s){
+//             if(ch == '(' || ch == '{' || ch == '[')
+//                 st.push(ch);
+//             else if(ch == ')' && !st.empty() && st.top() == '(')
+//                 st.pop();
+//             else if(ch == '}' && !st.empty() && st.top() == '{')
+//                 st.pop();
+//             else if(ch == ']' && !st.empty()  && st.top() == '[')
+//                 st.pop();
+//             else 
+//                 return false;
+//         }
+
+//         return st.empty() ? true : false;
+//     }
+// };
