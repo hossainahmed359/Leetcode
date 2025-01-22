7ca2837b3bdb5c74d9fd143e7a060216ecf3960e
Hossain Ahmed Madani <70832248+hossainahmed359@users.noreply.github.com>
2025-01-23 02:09:05 +0600

    Time: 0 ms (100.00%) | Memory: 9.9 MB (86.17%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -1,17 +1,35 @@
-
+// USING SLOW FAST
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        int len = 0;
-        for (ListNode* i = head; i != NULL; i = i->next) {
-            len++;
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        ListNode* curr = head;
-        for (int i = 0; i < len / 2; i++) {
-            curr = curr->next;
-        }
-
-        return curr;
+        return slow;
     }
 };
+
+
+
+// USING FOR LOOP
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+//         int len = 0;
+//         for (ListNode* i = head; i != NULL; i = i->next) {
+//             len++;
+//         }
+
+//         ListNode* curr = head;
+//         for (int i = 0; i < len / 2; i++) {
+//             curr = curr->next;
+//         }
+
+//         return curr;
+//     }
+// };
