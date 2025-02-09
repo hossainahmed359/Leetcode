228fad653207f61eca805a657abc494df5d1a825
Hossain Ahmed Madani <70832248+hossainahmed359@users.noreply.github.com>
2025-02-10 00:11:06 +0600

    Time: 51 ms (41.59%) | Memory: 95.6 MB (67.75%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.cpp
+++ 2035-count-sub-islands/count-sub-islands.cpp
@@ -35,6 +35,7 @@ public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         n = grid2.size();
         m = grid2[0].size();
+        memset(freq, false, sizeof(freq));
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
